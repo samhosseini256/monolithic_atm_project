0b1fc8caafb761d987981358ed457c074572561c
Aziz Hosseini <shosseini@payeshgaran.net>
2023-02-15 12:00:10 +0330

    Initial commit

---
+++ src/main/java/com/example/atm/MonolithicAtmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.atm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolithicAtmProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonolithicAtmProjectApplication.class, args);
+    }
+
+}
